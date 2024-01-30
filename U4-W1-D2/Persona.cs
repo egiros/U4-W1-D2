93d394bba5e50f23e3c5f13db617dcaf2d48a6e5
egiros <148718730+egiros@users.noreply.github.com>
2024-01-30 15:48:57 +0100

    up

---
+++ U4-W1-D2/Persona.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_W1_D2
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+
+
+
+        public Persona(string nome, string cognome, int eta)
+        {
+            this.Nome = nome;
+            this.Cognome = cognome;
+            this.Eta = eta;
+
+        }
+
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public string GetCognome()
+        {
+            return Cognome;
+        }
+
+
+        public int GetEta()
+        {
+            return Eta;
+        }
+
+
+        public string GetDettagli()
+        {
+            string messaggio = Nome + "\t" + Cognome + "\t" + "Anni: " + Eta;
+            return messaggio;
+        }
+
+    }
+
+  
+}
+
---
+++ U4-W1-D2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_W1_D2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Persona persona1 = new Persona("Mario", "Rossi", 35);
+            persona1.GetNome();
+            persona1.GetCognome();
+            persona1.GetEta();
+            Console.WriteLine("Risultato:\n" + persona1.GetDettagli());
+
+
+            Console.WriteLine("\n\n\nInserire Nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Inserire Cognome: ");
+            string cognome = Console.ReadLine();
+            Console.WriteLine("Inserire Età: ");
+            string eta = Console.ReadLine();
+
+            Console.WriteLine("\n\nHai inserito: \nNome: " + nome + "\nCognome: " + cognome + "\nAnni: " + eta);
+
+            Console.ReadLine();
+
+        }
+    }
+}
